fbedf1244725c971e9457dbd68683eddefe2a071
Mick-gor <mgorski421@gmail.com>
2024-06-22 12:33:49 +0200

    fix button component

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -3,20 +3,24 @@ import Description from "./components/description/Description";
 import Title from "./components/title/Title";
 import { MOCK } from "./data.mock";
 
-const App = () => (
-  <main className='main-container'>
-    <div className='content-container'>
-      <div className='title-container'>
-        <Title color='black'>{MOCK.title}</Title>
+const App = () => {
+  return (
+    <main className='main-container'>
+      <div className='content-container'>
+        <div className='title-container'>
+          <Title color='black'>{MOCK.title}</Title>
+        </div>
+        <div className='description-container'>
+          <Description>{MOCK.description}</Description>
+        </div>
+        <div className='button-container'>
+          <Button type='button' color='black' onClick={() => {}}>
+            {MOCK.buttonOpen}
+          </Button>
+        </div>
       </div>
-      <div className='description-container'>
-        <Description>{MOCK.description}</Description>
-      </div>
-      <div className='button-container'>
-        <Button type='button'>{MOCK.button}</Button>
-      </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
 
 export default App;
--- react-project/src/components/button/Button.jsx
+++ react-project/src/components/button/Button.jsx
@@ -1,5 +1,7 @@
-const Button = (props) => (
-  <button className='button-content'>{props.children}</button>
-);
-
+const Button = (props) => { 
+    const backgroundColor = props.color === "white" ? "button-content-modal" : "";
+    return (
+        <button className={`button-content ${backgroundColor}`} onClick={props.onClick}>{props.children}</button>
+    );
+};
 export default Button;
