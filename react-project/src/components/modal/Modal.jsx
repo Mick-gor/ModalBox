895ba91ec80a27b71ab5f610b9d11213b96ea040
Mick-gor <mgorski421@gmail.com>
2024-06-26 21:04:08 +0200

    add button functionality

--- react-project/src/components/modal/Modal.jsx
+++ react-project/src/components/modal/Modal.jsx
@@ -1,15 +1,14 @@
 import Button from "../button/Button";
 import Title from "../title/Title";
-import { handleButtonOnClick } from "../../utils.js";
 import { MOCK } from "../../data.mock";
 
-const Modal = () => (
+const Modal = (props) => (
   <div className='modal-container'>
     <div className='title-container'>
       <Title color='white'>{MOCK.title}</Title>
     </div>
     <div className='button-modal-container'>
-      <Button type='button' variant='white' onClick={handleButtonOnClick}>
+      <Button type='button' variant='white' onClick={props.handleButtonClick}>
         {MOCK.buttonClose}
       </Button>
     </div>
--- react-project/src/pages/home.jsx
+++ react-project/src/pages/home.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import Button from "../components/button/Button";
 import Description from "../components/description/Description";
 import Title from "../components/title/Title";
-import { handleButtonOnClick } from "../utils.js";
+import Modal from "../components/modal/Modal";
 import { MOCK } from "../data.mock";
 
 export const Home = () => {
+  const [modal, modalHandler] = useState(false);
+
+  const toggleModal = () => {
+    modalHandler(!modal);
+  };
+
   return (
     <div className='main-container'>
+      {modal && (
+        <div className='modal-overlay'>
+          <div className='backdrop' />
+          <Modal handleButtonClick={toggleModal} />
+        </div>
+      )}
+
       <div className='content-container'>
         <div className='title-container'>
           <Title color='black'>{MOCK.title}</Title>
@@ -15,7 +29,7 @@ export const Home = () => {
           <Description>{MOCK.description}</Description>
         </div>
         <div className='button-container'>
-          <Button type='button' variant='black' onClick={handleButtonOnClick}>
+          <Button type='button' variant='black' onClick={toggleModal}>
             {MOCK.buttonOpen}
           </Button>
         </div>
