57e9d7635b38d0b93dc98ee3a0fe4b41de965723
Mick-gor <mgorski421@gmail.com>
2024-06-26 19:57:26 +0200

    fix modal component

--- react-project/src/components/modal/Modal.jsx
+++ react-project/src/components/modal/Modal.jsx
@@ -4,17 +4,16 @@ import { handleButtonOnClick } from "../../utils.js";
 import { MOCK } from "../../data.mock";
 
 const Modal = () => (
-    <div className="modal-container">
-        <div className="title-container">
-            <Title color='white'>{MOCK.title}</Title>
-        </div>
-        <div className="button-modal-container">
-        <Button type='button' variant='white' onClick={handleButtonOnClick}>
-            {MOCK.buttonClose}
-          </Button>
-        </div>
+  <div className='modal-container'>
+    <div className='title-container'>
+      <Title color='white'>{MOCK.title}</Title>
     </div>
+    <div className='button-modal-container'>
+      <Button type='button' variant='white' onClick={handleButtonOnClick}>
+        {MOCK.buttonClose}
+      </Button>
+    </div>
+  </div>
 );
-  
-  export default Modal;
-  
+
+export default Modal;
