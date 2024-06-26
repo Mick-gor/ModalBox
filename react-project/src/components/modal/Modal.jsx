82ad6129e75e2f8b144cec187bc168106160ebad
Mick-gor <mgorski421@gmail.com>
2024-06-26 19:28:02 +0200

    add modal component

---
+++ react-project/src/components/modal/Modal.jsx
@@ -0,0 +1,20 @@
+import Button from "../button/Button";
+import Title from "../title/Title";
+import { handleButtonOnClick } from "../../utils.js";
+import { MOCK } from "../../data.mock";
+
+const Modal = () => (
+    <div className="modal-container">
+        <div className="title-container">
+            <Title color='white'>{MOCK.title}</Title>
+        </div>
+        <div className="button-modal-container">
+        <Button type='button' variant='white' onClick={handleButtonOnClick}>
+            {MOCK.buttonClose}
+          </Button>
+        </div>
+    </div>
+);
+  
+  export default Modal;
+  
